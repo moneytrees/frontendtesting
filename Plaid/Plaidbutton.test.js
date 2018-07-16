a2b25f04970471f523411e873ed4822d8e28e357
Carolina RC <caro.rmz315@gmail.com>
2018-07-16 11:00:35 -0500

    ensuring Plaid test works

--- Plaid/Plaidbutton.test.js
+++ Plaid/Plaidbutton.test.js
@@ -1,29 +1,29 @@
 // Ensuring plaid button renders upon screen load 
 
 // created mock localStorage 
-class LocalStorageMock {
-    constructor() {
-      this.store = {};
-    }
+// class LocalStorageMock {
+//     constructor() {
+//       this.store = {};
+//     }
   
-    clear() {
-      this.store = {};
-    }
+//     clear() {
+//       this.store = {};
+//     }
   
-    getItem(key) {
-      return this.store[key] || null;
-    }
+//     getItem(key) {
+//       return this.store[key] || null;
+//     }
   
-    setItem(key, value) {
-      this.store[key] = value.toString();
-    }
+//     setItem(key, value) {
+//       this.store[key] = value.toString();
+//     }
   
-    removeItem(key) {
-      delete this.store[key];
-    }
-  };
+//     removeItem(key) {
+//       delete this.store[key];
+//     }
+//   };
   
-  global.localStorage = new LocalStorageMock;
+//   global.localStorage = new LocalStorageMock;
 
 import React from 'react';
 import { shallow, mount,render } from 'enzyme';
